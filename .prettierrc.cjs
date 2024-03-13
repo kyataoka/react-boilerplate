9e319edd990d1df193429a42870ea3e92b402d27
kyataoka <kyataoka.deve@gmail.com>
2024-03-13 19:51:08 +0000

    add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  // printWidth: 80,
+  // tabWidth: 2,
+  // useTabs: false,
+  // semi: true,
+  singleQuote: true, //false
+  // quoteProps: 'as-needed',
+  // jsxSingleQuote: false,
+  // trailingComma: 'all',
+  // bracketSpacing: true
+  // bracketSameLine: false,
+  // arrowParens: 'always',
+  // rangeStart: 0,
+  // rangeEnd: Infinity,
+  // parser: 'babel',
+  // filepath: 'index.js',
+  // requirePragma: false,
+  // insertPragma: false,
+  // proseWrap: 'preserve',
+  // htmlWhitespaceSensitivity: 'css',
+  // vueIndentScriptAndStyle: false,
+  // endOfLine: 'lf',
+  // embeddedLanguageFormatting: 'auto'
+  singleAttributePerLine: true, //false
+};
