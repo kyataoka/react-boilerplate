366bddbed4df8ae832bb63107cdded114b610449
kyataoka <kyataoka.deve@gmail.com>
2024-03-14 05:53:25 +0000

    remove unnecessary templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import './App.css';
 import { useCallback, useState } from 'react';
-
-import reactLogo from './assets/react.svg';
-
-import viteLogo from '/vite.svg';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -14,40 +9,10 @@ const App = () => {
 
   return (
     <>
+      <h1>React boilerplate</h1>
       <div>
-        <a
-          href="https://vitejs.dev"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
         <button onClick={handleClick}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 };
